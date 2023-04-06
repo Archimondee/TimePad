aa86d762ac667b34a6be56857eb991c50cf2a0fb
Archimondee <kikchiemedia@gmail.com>
2023-04-07 01:51:59 +0700

    Initial Commit

---
+++ TimePad/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TimePad
+//
+//  Created by Gilang Aditya Rahman on 07/04/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
